6301b101d4a28131ccf838334d58a8d7effe9adb
ahmadsebak <radio_frequency@hotmail.com>
2015-03-26 17:59:55 -0400

    delete build files

--- build.sh
+++
@@ -1,4 +0,0 @@
-mvn clean \
-com.savage7.maven.plugins:maven-external-dependency-plugin:resolve-external \
-com.savage7.maven.plugins:maven-external-dependency-plugin:install-external
-mvn package "$@"
