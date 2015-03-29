f403708c1de6c67c791de86359e76c5940f269ec
ahmadsebak <radio_frequency@hotmail.com>
2015-03-29 16:12:02 -0400

    Fixed up running and debuggin configurations mixing

--- src/com/atsebak/raspberrypi/runner/RaspberryPIRunner.java
+++ src/com/atsebak/raspberrypi/runner/RaspberryPIRunner.java
@@ -2,15 +2,10 @@ package com.atsebak.raspberrypi.runner;
 
 import com.atsebak.raspberrypi.console.PIConsoleToolWindowFactory;
 import com.atsebak.raspberrypi.console.PIConsoleView;
-import com.intellij.execution.*;
-import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
-import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.executors.DefaultRunExecutor;
-import com.intellij.execution.process.ProcessHandler;
-import com.intellij.execution.remote.RemoteConfiguration;
-import com.intellij.execution.remote.RemoteConfigurationType;
 import com.intellij.execution.runners.DefaultProgramRunner;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.ui.RunContentDescriptor;
@@ -18,31 +13,14 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.content.Content;
-import com.intellij.util.NotNullFunction;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
 
 public class RaspberryPIRunner extends DefaultProgramRunner {
     private static final String RUNNER_ID = "RaspberryPIRunner";
-    @NonNls
-    private static final String RUN_CONFIGURATION_NAME_PATTERN = "PI Debugger (%s)";
 
     /**
-     * Gets the debug runner
-     *
-     * @param debugPort
-     * @return
-     */
-    @NotNull
-    private static String getRunConfigurationName(String debugPort) {
-        return String.format(RUN_CONFIGURATION_NAME_PATTERN, debugPort);
-    }
-
-    /**
-     * Executes the Runner, This only gets called in debug mode
+     * Executes the Runner, This only gets called in run mode.
+     * It returns null because you want to show only the PI Console
      *
      * @param profileState
      * @param environment
@@ -52,12 +30,10 @@ public class RaspberryPIRunner extends DefaultProgramRunner {
     @Override
     protected RunContentDescriptor doExecute(@NotNull RunProfileState profileState, @NotNull ExecutionEnvironment environment) throws ExecutionException {
         final RunProfile runProfileRaw = environment.getRunProfile();
-        FileDocumentManager.getInstance().saveAllDocuments();
         if (runProfileRaw instanceof RaspberryPIRunConfiguration) {
-            RaspberryPIRunnerParameters parameters = ((RaspberryPIRunConfiguration) runProfileRaw).getRunnerParameters();
-            closeOldSessionAndRun(environment.getProject(), parameters);
+            FileDocumentManager.getInstance().saveAllDocuments();
+            setupConsole(environment.getProject());
         }
-        setupConsole(environment.getProject());
         return null;
     }
 
@@ -80,83 +56,7 @@ public class RaspberryPIRunner extends DefaultProgramRunner {
      */
     @Override
     public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
-        return (DefaultDebugExecutor.EXECUTOR_ID.equals(executorId) || DefaultRunExecutor.EXECUTOR_ID.equals(executorId)) &&
-                profile instanceof RaspberryPIRunConfiguration;
-    }
-
-    /**
-     * Creates debugging settings for server
-     *
-     * @param project
-     * @param debugPort
-     * @param hostname
-     * @return
-     */
-    private RunnerAndConfigurationSettings createRunConfiguration(Project project, String debugPort, String hostname) {
-        final RemoteConfigurationType remoteConfigurationType = RemoteConfigurationType.getInstance();
-
-        final ConfigurationFactory factory = remoteConfigurationType.getFactory();
-        final RunnerAndConfigurationSettings runSettings =
-                RunManager.getInstance(project).createRunConfiguration(getRunConfigurationName(debugPort), factory);
-        final RemoteConfiguration configuration = (RemoteConfiguration) runSettings.getConfiguration();
-
-        configuration.HOST = hostname;
-        configuration.PORT = debugPort;
-        configuration.USE_SOCKET_TRANSPORT = true;
-        configuration.SERVER_MODE = false;
-
-        return runSettings;
-    }
-
-
-    /**
-     * Closes an old descriptor and creates a new one
-     *
-     * @param project
-     * @param parameters
-     */
-    private void closeOldSessionAndRun(final Project project, RaspberryPIRunnerParameters parameters) {
-        final String configurationName = getRunConfigurationName(parameters.getPort());
-        final Collection<RunContentDescriptor> descriptors =
-                ExecutionHelper.findRunningConsoleByTitle(project, new NotNullFunction<String, Boolean>() {
-                    @NotNull
-                    @Override
-                    public Boolean fun(String title) {
-                        return configurationName.equals(title);
-                    }
-                });
-
-        if (descriptors.size() > 0) {
-            final RunContentDescriptor descriptor = descriptors.iterator().next();
-            final ProcessHandler processHandler = descriptor.getProcessHandler();
-            final Content content = descriptor.getAttachedContent();
-
-            if (processHandler != null && content != null) {
-                final Executor executor = DefaultDebugExecutor.getDebugExecutorInstance();
-
-                if (processHandler.isProcessTerminated()) {
-                    ExecutionManager.getInstance(project).getContentManager()
-                            .removeRunContent(executor, descriptor);
-                } else {
-                    content.getManager().setSelectedContent(content);
-                    ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(executor.getToolWindowId());
-                    window.activate(null, false, true);
-                    return;
-                }
-            }
-        }
-        runSession(project, parameters);
-    }
-
-    /**
-     * Runs in remote debug mode using that executioner
-     *
-     * @param project
-     * @param parameters
-     */
-    private void runSession(final Project project, RaspberryPIRunnerParameters parameters) {
-        final RunnerAndConfigurationSettings settings = createRunConfiguration(project, parameters.getPort(), parameters.getHostname());
-        ProgramRunnerUtil.executeConfiguration(project, settings, DefaultDebugExecutor.getDebugExecutorInstance());
+        return (DefaultRunExecutor.EXECUTOR_ID.equals(executorId)) && profile instanceof RaspberryPIRunConfiguration;
     }
 
     /**
@@ -171,5 +71,4 @@ public class RaspberryPIRunner extends DefaultProgramRunner {
             PIConsoleView.getInstance(p).clear();
         }
     }
-
 }
