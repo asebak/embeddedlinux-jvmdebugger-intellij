8e319977d94d68eeafd65f01a8d64e1b2bae4c15
ahmad sebak <rhymeweaponlegend@gmail.com>
2015-04-01 18:26:37 -0400

    update build

---
+++ ci-build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ideaVersion="14.1"
+if [ ! -d ./idea-IC ]; then
+# Get our IDEA dependency
+if [ -f ~/Tools/ideaIC-${ideaVersion}.tar.gz ];
+then
+cp ~/Tools/ideaIC-${ideaVersion}.tar.gz .
+else
+wget http://download.jetbrains.com/idea/ideaIC-${ideaVersion}.tar.gz
+# wget http://download.labs.intellij.net/idea/ideaIC-${ideaVersion}.tar.gz
+fi
+# Unzip IDEA
+tar zxf ideaIC-${ideaVersion}.tar.gz
+rm -rf ideaIC-${ideaVersion}.tar.gz
+# Move the versioned IDEA folder to a known location
+ideaPath=$(find . -name 'idea-IC*' | head -n 1)
+mv ${ideaPath} ./idea-IC
+fi
+
+
+# Tests
+if [ "$1" = "-d" ]; then
+ant -d -f tests.xml -DIDEA_HOME=./idea-IC
+else
+ant -f tests.xml -DIDEA_HOME=./idea-IC
+fi
+# Build Status
+stat=$?
+if [ "${TRAVIS}" != true ]; then
+ant -f tests.xml -q clean
+rm -rf idea-IC
+fi
+# build status
+exit ${stat}
